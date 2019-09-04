0b7d27899a2d722d6246038033c073bf8521dea9
Roy Zhang <roy.zhang@stinkstudiosworks.asia>
2019-09-04 17:59:39 +0800

    all good

---
+++ CourseCode/3.12_String.py
@@ -0,0 +1,7 @@
+name = "Sam"
+print(name)
+print(name[0:1])
+print(name[1:2])
+print(name[2:3])
+print(name.upper())
+print(name)
--- CourseCode/3.2_Numbers.py
+++ CourseCode/3.2_Numbers.py
@@ -3,3 +3,4 @@ print(2-1)
 print(2*2)
 print(8/2)
 print(38%2)
+print(2**3) #this is 2 to the power of 3#
